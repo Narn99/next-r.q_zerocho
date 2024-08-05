0f1537c684100753545648c2ef0bfd4754f75a0c
Narn99 <vkdldjtmflr@naver.com>
2024-08-05 11:41:30 +0900

    router.replace, css

--- ch0/src/app/layout.tsx
+++ ch0/src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-import styles from "./page.module.css";
+import styles from "./(beforeLogin)/@modal/_component/Main";
 
 const inter = Inter({ subsets: ["latin"] });
 
